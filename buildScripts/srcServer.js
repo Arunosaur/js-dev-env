2a52e8644e6817c9479b75e49459996454d0b92c
Arun Swaminathan Rajagopalan <Arun.Rajagopalan@mclaneco.com>
2020-05-09 22:35:00 -0500

    Added fetch and mock API

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -15,11 +15,27 @@ app.use(require('webpack-dev-middleware')(compiler, {
    publicPath: config.output.publicPath
 }));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err) {
+app.get('/users', function (req, res) {
+   //Hard coding
+   res.json([
+      {"id": 1, "firstName": "Ashwin", "lastName": "Swaminathan", "email": "ashwins@gmail.com"},
+      {"id": 2, "firstName": "Ananya", "lastName": "Swaminathan", "email": "ananyas@gmail.com"},
+      {"id": 3, "firstName": "Radhika", "lastName": "Balasubramanian", "email": "radhi.arun@gmail.com"},
+      {"id": 4, "firstName": "Vijayalakshmi", "lastName": "Rajagopalan", "email": "vijiraja@gmail.com"},
+      {"id": 5, "firstName": "Rajagopalan", "lastName": "Krishnamoorthy", "email": "krajagopalan1k@hotmail.com"},
+      {"id": 6, "firstName": "John", "lastName": "Rice", "email": "j2rice@gmail.com"},
+      {"id": 7, "firstName": "Paul", "lastName": "Le", "email": "ple4096@gmail.com"},
+      {"id": 8, "firstName": "Duraiarasan", "lastName": "Arumugam", "email": "mynanba@gmail.com"},
+      {"id": 9, "firstName": "Ranganathan", "lastName": "Venukrishnan", "email": "rangarao@gmail.com"},
+      {"id": 10, "firstName": "Krishnakumar", "lastName": "Sampath", "email": "kawareness@yahoo.com"},
+   ])
+});
+
+app.listen(port, function (err) {
    if (err) {
       console.log(err);
    } else {
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,7 @@
 import {expect} from 'chai';
 import jsdom from 'jsdom';
 import fs from 'fs';
+import {describe} from "mocha";
 
 describe('Our first test', () => {
    it('should pass', () => {
@@ -9,13 +10,13 @@ describe('Our first test', () => {
 })
 
 describe('index.html', () => {
-   it('it should say hello', (done) => {
+   it('it should say users', (done) => {
       const index = fs.readFileSync('./src/index.html', "utf-8");
       const {JSDOM} = jsdom;
       const {window} = new JSDOM();
       const {document} = (new JSDOM(index)).window;
       const h1 = document.getElementsByTagName("h1")[0];
-      expect(h1.innerHTML).to.equal("Hello, World!");
+      expect(h1.innerHTML).to.equal("Users");
       done();
       window.close();
    })
